731249eeab375e6f8468c7d2663a85b01a0c53ca
Gustavo Branco <guster32@gmail.com>
2022-09-13 22:06:19 -0700

    Build with no optimizations for remote debugging.

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,10 @@ pushd ${SRC_DIR}/crossbuild/debug
 rm -rf ${SRC_DIR}/crossbuild/debug/* 
 
 #Cmake
-cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ../../
+cmake -DCMAKE_BUILD_TYPE=Debug \
+-DCMAKE_C_FLAGS_DEBUG="-g -O0" \
+-DCMAKE_CXX_FLAGS_DEBUG="-g -O0" \
+-GNinja ../../
 #-DCMAKE_CXX_COMPILER=${config:ARCADIA_QEMU.CXX}
 #-DCMAKE_C_COMPILER=${config:ARCADIA_QEMU.CC}
 #-DCMAKE_SYSROOT=${config:ARCADIA_QEMU.SDKTARGETSYSROOT}
