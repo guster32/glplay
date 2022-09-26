d45331f4c5973759d4d98aac8bbfc97296484b84
Gustavo Branco <guster32@gmail.com>
2022-09-25 17:06:28 -0700

    Complete VT setup

--- src/glplay/main.cpp
+++ src/glplay/main.cpp
@@ -2,6 +2,7 @@
 #include "../drm/drm.hpp"
 #include "../gbm/gbm.hpp"
 #include "../egl/egl.hpp"
+#include "../nix/nix.hpp"
 #include <iostream>
 
 auto main(int argc, char *argv[]) -> int {
@@ -10,6 +11,8 @@ auto main(int argc, char *argv[]) -> int {
 	auto buffer = glplay::gbm::make_gbm_ptr(adapter.getAdapterFD());
 	//Create renderer here  vk_device_create or device_egl_setup or software
 	auto *display = glplay::egl::device_egl_setup(buffer.get(), adapter.hasfbModifiersSupport());
+	auto vt_fd = glplay::nix::vt_setup();
+	glplay::nix::vt_reset(vt_fd);
   
 	return 0;
 }
--- src/nix/VirtualTerminal.hpp
+++ src/nix/VirtualTerminal.hpp
@@ -44,13 +44,15 @@
  *
  * Author: Daniel Stone <daniels@collabora.com>
  */
-
+#pragma once
+#include <bits/types/FILE.h>
 #include <cassert>
 #include <cerrno>
 #include <fcntl.h>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <string>
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -60,115 +62,105 @@
 #include <linux/kd.h>
 #include <linux/major.h>
 #include <linux/vt.h>
+#include <array>
 
+#include "FileDescriptor.hpp"
 #include "log.hpp"
 
 
-/*
- * Set up the VT/TTY so it runs in graphics mode and lets us handle our own
- * input. This uses the VT specified in $TTYNO if specified, or the current VT
- * if we're running directly from a VT, or failing that tries to find a free
- * one to switch to.
- */
-static int vt_setup(struct device *device)
-{
-	const char *tty_num_env = getenv("TTYNO");
-	int tty_num = 0;
-	int cur_vt;
-	char tty_dev[32];
+namespace glplay::nix {
+	/*
+	* Set up the VT/TTY so it runs in graphics mode and lets us handle our own
+	* input. This uses the VT specified in $TTYNO if specified, or the current VT
+	* if we're running directly from a VT, or failing that tries to find a free
+	* one to switch to.
+	*/
+	inline auto static vt_setup() -> FileDescriptor {
+		int tty_num = 0;
+		int cur_vt = 0;
+		
+		std::array<char, 32> tty_dev{};
 
-	/* If $TTYNO is set in the environment, then use that first. */
-	if (tty_num_env) {
-		char *endptr = NULL;
+		/* if we're running from a VT ourselves, just reuse
+		* Other-other-wise, look for a free VT we can use by querying /dev/tty0.
+		*/
+		if (ttyname(STDIN_FILENO) != nullptr) {
+			ttyname_r(STDIN_FILENO, tty_dev.data(), tty_dev.size());
+		} else {
+			std::string tty0_str = "/dev/tty0";
+			auto tty0 = FileDescriptor(tty0_str, O_WRONLY | O_CLOEXEC);
+			if (tty0.fileDescriptor() < 0) {
+				fprintf(stderr, "couldn't open /dev/tty0\n");
+				throw std::runtime_error("Failed to open TTY");
+			}
 
-		tty_num = strtoul(tty_num_env, &endptr, 10);
-		if (tty_num == 0 || *endptr != '\0') {
-			fprintf(stderr, "invalid $TTYNO environment variable\n");
-			return -1;
-		}
-		snprintf(tty_dev, sizeof(tty_dev), "/dev/tty%d", tty_num);
-	} else if (ttyname(STDIN_FILENO)) {
-		/* Otherwise, if we're running from a VT ourselves, just reuse
-		 * that. */
-		ttyname_r(STDIN_FILENO, tty_dev, sizeof(tty_dev));
-	} else {
-		int tty0;
-
-		/* Other-other-wise, look for a free VT we can use by querying
-		 * /dev/tty0. */
-		tty0 = open("/dev/tty0", O_WRONLY | O_CLOEXEC);
-		if (tty0 < 0) {
-			fprintf(stderr, "couldn't open /dev/tty0\n");
-			return -errno;
+			if (ioctl(tty0.fileDescriptor(), VT_OPENQRY, &tty_num) < 0 || tty_num < 0) {
+				fprintf(stderr, "couldn't get free TTY\n");
+				throw std::runtime_error("Failed to free TTY");
+			}
+			sprintf(tty_dev.data(), "/dev/tty%d", tty_num);
 		}
 
-		if (ioctl(tty0, VT_OPENQRY, &tty_num) < 0 || tty_num < 0) {
-			fprintf(stderr, "couldn't get free TTY\n");
-			close(tty0);
-			return -errno;
-		}
-		close(tty0);
-		sprintf(tty_dev, "/dev/tty%d", tty_num);
-	}
 
-	device->vt_fd = open(tty_dev, O_RDWR | O_NOCTTY);
-	if (device->vt_fd < 0) {
-		fprintf(stderr, "failed to open VT %d\n", tty_num);
-		return -errno;
-	}
-
-	/* If we get our VT from stdin, work painfully backwards to find its
-	 * VT number. */
-	if (tty_num == 0) {
-		struct stat buf;
-
-		if (fstat(device->vt_fd, &buf) == -1 ||
-		    major(buf.st_rdev) != TTY_MAJOR) {
-			fprintf(stderr, "VT file %s is bad\n", tty_dev);
-			return -1;
+		auto test = std::string(tty_dev.data());
+		auto vt_fd = FileDescriptor(test, O_RDWR | O_NOCTTY);
+		if (vt_fd.fileDescriptor() < 0) {
+			fprintf(stderr, "failed to open VT %d\n", tty_num);
+			throw std::runtime_error("Failed to open VT");
 		}
 
-		tty_num = minor(buf.st_rdev);
+		/* If we get our VT from stdin, work painfully backwards to find its
+		* VT number. */
+		if (tty_num == 0) {
+			struct stat buf{};
+
+			if (fstat(vt_fd.fileDescriptor(), &buf) == -1 ||
+					major(buf.st_rdev) != TTY_MAJOR) {
+				fprintf(stderr, "VT file %s is bad\n", tty_dev.data());
+				throw std::runtime_error("Bad VT file");
+			}
+
+			tty_num = minor(buf.st_rdev);
+		}
+		assert(tty_num != 0);
+
+		printf("using VT %d\n", tty_num);
+
+		/* Switch to the target VT. */
+		if (ioctl(vt_fd.fileDescriptor(), VT_ACTIVATE, tty_num) != 0 ||
+				ioctl(vt_fd.fileDescriptor(), VT_WAITACTIVE, tty_num) != 0) {
+			fprintf(stderr, "couldn't switch to VT %d\n", tty_num);
+			throw std::runtime_error("Failed to switch to target VT");
+		}
+
+		debug("switched to VT %d\n", tty_num);
+
+		/* Completely disable kernel keyboard processing: this prevents us
+		* from being killed on Ctrl-C. */
+		// if (ioctl(vt_fd.fileDescriptor(), KDGKBMODE, &device->saved_kb_mode) != 0 ||
+		// 		ioctl(vt_fd.fileDescriptor(), KDSKBMODE, K_OFF) != 0) {
+		// 	fprintf(stderr, "failed to disable TTY keyboard processing\n");
+		// 	return -errno;
+		// }
+
+		/* Change the VT into graphics mode, so the kernel no longer prints
+		* text out on top of us. */
+		if (ioctl(vt_fd.fileDescriptor(), KDSETMODE, KD_GRAPHICS) != 0) {
+			fprintf(stderr, "failed to switch TTY to graphics mode\n");
+			throw std::runtime_error("Failed to switch to graphics mode");
+		}
+
+		debug("VT setup complete\n");
+
+		/* Normally we would also call VT_SETMODE to change the mode to
+		* VT_PROCESS here, which would allow us to intercept VT-switching
+		* requests and tear down KMS. But we don't, since that requires
+		* signal handling. */
+		return vt_fd;
 	}
-	assert(tty_num != 0);
 
-	printf("using VT %d\n", tty_num);
-
-	/* Switch to the target VT. */
-	if (ioctl(device->vt_fd, VT_ACTIVATE, tty_num) != 0 ||
-	    ioctl(device->vt_fd, VT_WAITACTIVE, tty_num) != 0) {
-		fprintf(stderr, "couldn't switch to VT %d\n", tty_num);
-		return -errno;
+	inline auto static vt_reset(FileDescriptor &vt_fd) ->void {
+		//ioctl(vt_fd.fileDescriptor(), KDSKBMODE, device->saved_kb_mode);
+		ioctl(vt_fd.fileDescriptor(), KDSETMODE, KD_TEXT);
 	}
-
-	debug("switched to VT %d\n", tty_num);
-
-	/* Completely disable kernel keyboard processing: this prevents us
-	 * from being killed on Ctrl-C. */
-	if (ioctl(device->vt_fd, KDGKBMODE, &device->saved_kb_mode) != 0 ||
-	    ioctl(device->vt_fd, KDSKBMODE, K_OFF) != 0) {
-		fprintf(stderr, "failed to disable TTY keyboard processing\n");
-		return -errno;
-	}
-
-	/* Change the VT into graphics mode, so the kernel no longer prints
-	 * text out on top of us. */
-	if (ioctl(device->vt_fd, KDSETMODE, KD_GRAPHICS) != 0) {
-		fprintf(stderr, "failed to switch TTY to graphics mode\n");
-		return -errno;
-	}
-
-	debug("VT setup complete\n");
-
-	/* Normally we would also call VT_SETMODE to change the mode to
-	 * VT_PROCESS here, which would allow us to intercept VT-switching
-	 * requests and tear down KMS. But we don't, since that requires
-	 * signal handling. */
-	return 0;
-}
-
-static void vt_reset(struct device *device)
-{
-	ioctl(device->vt_fd, KDSKBMODE, device->saved_kb_mode);
-	ioctl(device->vt_fd, KDSETMODE, KD_TEXT);
 }
